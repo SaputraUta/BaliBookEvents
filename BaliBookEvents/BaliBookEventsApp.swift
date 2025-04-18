e9638723511ef36ae59f86c84e815a6bbed7857d
Saputra <saputra.uta50@gmail.com>
2025-04-18 22:38:29 +0800

    Initial Commit

---
+++ BaliBookEvents/BaliBookEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaliBookEventsApp.swift
+//  BaliBookEvents
+//
+//  Created by Saputra on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BaliBookEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaliBookEvents/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BaliBookEvents
+//
+//  Created by Saputra on 18/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
